c3cb8eabad85cbbd7566c2ea010f3b899976720e
unknown <mennahisham1d@gmail.com>
2022-10-20 09:14:40 +0200

    mobile nav improvemments

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 const headerEl = document.querySelector(".header");
 const navBtn = document.querySelector(".btn-mobile-nav");
+const htmlEl =document.querySelector("html");
 
 
 navBtn.addEventListener("click",()=>{
     headerEl.classList.toggle("nav-open");
+    htmlEl.classList.toggle("hide-overflow");
 })
